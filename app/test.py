8c1d5c180eb38f30e3dd7eec1c64503b79620148
Projit Ghosh <projitg@nihilentanalytics.com>
2019-07-15 17:12:58 +0530

    removed test file

--- app/test.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-
-password = "test"
-hashed_password = generate_password_hash(password)
-print(hashed_password)
-l_password = hashed_password
-verify_password = check_password_hash(hashed_password, l_password)
-print(verify_password)
